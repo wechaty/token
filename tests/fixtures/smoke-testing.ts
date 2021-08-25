a58cd7b6fcaca1e041d42a9c510ca5d65e1053f6
Huan (李卓桓) <zixia@zixia.net>
2021-08-25 12:57:37 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -5,12 +5,18 @@ import {
   VERSION,
 }                 from 'wechaty-token'
 
+import assert from 'assert'
+
 async function main () {
   if (VERSION === '0.0.0') {
     throw new Error('version should not be 0.0.0 when prepare for publishing')
   }
 
-  const wechatyToken = new WechatyToken()
+  const wechatyToken = new WechatyToken('__sni__/__token__')
+
+  assert(wechatyToken.token === '__token__', 'token should be __token__')
+  assert(wechatyToken.sni === '__sni__', 'sni should be __sni__')
+
   console.info(`Puppet v${wechatyToken.version()} smoke testing passed.`)
   return 0
 }
